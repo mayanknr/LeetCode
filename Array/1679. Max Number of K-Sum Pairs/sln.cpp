3cf63bc40955df10a84f3daa5e2c852e5f5ffd68
Mayank Rangari <gr8mayu@gmail.com>
2022-07-24 03:04:03 +0530

    Update sln.cpp

--- Array/1679. Max Number of K-Sum Pairs/sln.cpp
+++ Array/1679. Max Number of K-Sum Pairs/sln.cpp
@@ -1,1 +1,32 @@
 
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int size=nums.size();
+        int maxOperations=0;
+        sort(nums.begin(),nums.end());
+    
+            int low=0;
+            int high=size-1;
+            while(low<high)
+            {
+                int mid=low+(high-low)/2;
+                
+                if(nums[low]+nums[high]==k)
+                {
+                    //cout<<nums[i]<<" "<<nums[mid]<<endl;
+                    maxOperations++;
+                    low++;
+                    high--;
+                    
+                }
+                else if(nums[low]+nums[high]>k)
+                    high--;
+                else
+                    low++;
+            }
+        
+        
+        return maxOperations;
+    }
+};
